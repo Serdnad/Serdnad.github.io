c65fefabdbda885c7bbe37b092e8a9e0817071b2
Andres Gutierrez <andresgutierrez535@gmail.com>
2017-04-10 22:31:22 -0400

    Move to SAS Website

--- functionCollection.js
+++
@@ -1,8 +0,0 @@
-function expandNav() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
-}
